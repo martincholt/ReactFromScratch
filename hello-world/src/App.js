dbdb67eca8ee1f161189261fed75c96881eefa79
martincholt <martindomingo25@gmail.com>
2022-06-19 12:42:48 +0200

    created components + props + states

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,12 +1,24 @@
 import "./App.css";
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Hello from "./components/Hello";
+import Message from "./components/Message";
 
 function App() {
   return (
     <div className="App">
-      <Greet />
-      <Welcome />
+      <Message/>
+      {/* <Greet name="Martin" heroName="Sirius">
+        <p>This is children props</p>
+      </Greet>
+      <Greet name="Cris" heroName="Hermione">
+        <button>Action</button>
+      </Greet>
+      <Greet name="Lobita" heroName="Hedwig"/>
+      <Welcome name="Martin" heroName="Sirius"/>
+      <Welcome name="Cris" heroName="Hermione"/>
+      <Welcome name="Lobita" heroName="Hedwig"/>
+      <Hello /> */}
     </div>
   );
 }
